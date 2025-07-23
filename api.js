1bdd76452dfe6d864a8139e20d98642677dabfc1
Francisco-Bittencourt <francisco.bittencourt@ba.estudante.senai.br>
2025-07-23 09:55:13 -0300

    add cors options

--- api.js
+++ api.js
@@ -7,7 +7,12 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:4200"],
+  methods: ["GET", "PUT", "POST", "DELETE"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+  credentials: true,
+}));
 
 app.use(express.json());
 
