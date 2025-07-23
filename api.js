df6c7779a3b1565cc1208b4564bb4eea031a0db2
Francisco-Bittencourt <francisco.bittencourt@ba.estudante.senai.br>
2025-07-23 08:49:58 -0300

    improve code

--- api.js
+++ api.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
+const dotenv = require("dotenv")
+const path = require("path");
+
+dotenv.config()
 
 const app = express();
-
+const PORT = process.env.PORT || 3000
 app.use(cors());
 
 app.use(express.json());
